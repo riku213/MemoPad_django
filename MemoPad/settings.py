5b10553c0a5485eef85606d26ee666f165cf0279
riku213 <kotatorameme3@gmail.com>
2026-04-26 16:25:12 +0900

    fix: MemoPad/settings.py memo.apps.MemoConfig追加

--- MemoPad/settings.py
+++ MemoPad/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'memo.apps.MemoConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
