94fcf6c4de55e496638964c3c12d9a882b24f86f
riku213 <kotatorameme3@gmail.com>
2026-04-26 16:20:43 +0900

    feat: memo/models.py モデルの生成

--- memo/models.py
+++ memo/models.py
@@ -1,3 +1,10 @@
-from django.db import models
+from django.db import db
 
-# Create your models here.
+class Memo(db.Model):
+    # メモの本文（長いテキストを許可）
+    text = db.TextField()
+    # 更新日時（保存時に自動で現在時刻を入れる）
+    updated_at = db.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.text
