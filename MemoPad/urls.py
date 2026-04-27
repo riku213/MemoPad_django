c6f02800bc61967bf2b5f9cc55539bd2c59f1152
riku213 <kotatorameme3@gmail.com>
2026-04-27 13:22:42 +0900

    fix: テンプレートエラーの修正

--- MemoPad/urls.py
+++ MemoPad/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include 
+from memo.views.views import Home # Homeビューをインポート
 
 urlpatterns = [
-    path('memo/', include('memo.urls')), 
     path('admin/', admin.site.urls),
+    path('', Home.as_view(), name='home'), # サイトのトップページ
+    path('memo/', include('memo.urls')), 
 ]
--- memo/urls.py
+++ memo/urls.py
@@ -1,7 +1,13 @@
 from django.urls import path
-from . import views
+from memo.views import views
+from memo.views.memo.views import MemoListView, MemoCreateView, MemoUpdateView, MemoDeleteView
 
 app_name = 'memo'
 urlpatterns = [
-    path('', views.index, name='index'),
+    # 各機能（CRUD）に対応するURLパス 
+    # path('', views.index, name='index'),
+    path('memos', MemoListView.as_view(), name='list_memo'),
+    path('memos_edit/<int:pk>', MemoUpdateView.as_view(), name='edit_memo'),
+    path('memos_new', MemoCreateView.as_view(), name='create_memo'),
+    path('memos_delete/<int:pk>', MemoDeleteView.as_view(), name='delete_memo'),
 ]
